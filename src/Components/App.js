4ef86425f8a6079c940788357cf966b06caf0e72
TalabH <thhussei@asu.edu>
2017-02-21 07:23:55 -0700

    Set up the FrontEnd React App

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+	state = {
+		pageHeader: 'SolarCity'
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<Header message={this.state.pageHeader} />
+				<div>
+				...
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
