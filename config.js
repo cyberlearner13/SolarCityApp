27b85b28519034a45e2dedfc000010c01d41d015
TalabH <thhussei@asu.edu>
2017-02-24 09:32:39 -0700

    added animations

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const env = process.env;
 export const nodeEnv = env.NODE_ENV || 'development';
 
 export default {
-	mongodbUri: 'mongodb://127.0.0.1:27017/test',
+	mongodbUri: 'mongodb://admin:admin@ds161059.mlab.com:61059/solarcity',
 	port: env.PORT || 8080,
 	host: env.HOST || '127.0.0.1',
 	get serverUrl() {
